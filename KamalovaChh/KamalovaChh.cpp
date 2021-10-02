33288382e8889c8c27994142a270c2d44d2dc4fa
Chulpan15 <ch_kamalova@mail.ru>
2021-10-02 21:28:48 +0500

    Добавлена структура "CompressionStation", добавлены характеристики в структура "Pipe"

--- KamalovaChh/KamalovaChh.cpp
+++ KamalovaChh/KamalovaChh.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+using namespace std;
+
 struct Pipe
 {
 	int id;
+	int diametr;
 	double length;
+	bool InRepair = false;
+};
+
+struct CompressionStation
+{
+	int id = 0;
+	string name;
+	int NumberOfWorkshops;
+	int NumberOfWorkshopsInOperation;
+	double effiency;
 
 };
+
 void PrintPipe(Pipe& p)
 {
-	std::cout << "You entered" << p.length << std::endl;
-}
+	std::cout << "You entered: " << p.length << std::endl;
+};
+
 Pipe NewPipe()
 {
 	Pipe p;
@@ -16,7 +31,8 @@ Pipe NewPipe()
 	std::cout << "Enter length: ";
 	std::cin >> p.length;
 	return p;
-}
+};
+
 int main()
 {
 	Pipe p = NewPipe();
