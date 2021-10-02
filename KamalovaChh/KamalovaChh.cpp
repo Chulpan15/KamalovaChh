acf052f71ce8ac897b7a208e37faa267ea3e86e8
Chulpan15 <ch_kamalova@mail.ru>
2021-10-03 00:00:02 +0500

    CreateCompSt + новые характеристики в PrintPipe

--- KamalovaChh/KamalovaChh.cpp
+++ KamalovaChh/KamalovaChh.cpp
@@ -7,7 +7,7 @@ struct Pipe
 	int id;
 	int diametr;
 	double length;
-	bool InRepair = true;
+	int InRepair;
 };
 
 struct CompressionStation
@@ -29,8 +29,13 @@ Pipe NewPipe()
 	cin >> p.diametr;
 	cout << "Please, enter length: ";
 	cin >> p.length;
-	cout << "Is pipe in repair?";
-	cin >> p.InRepair;
+	do
+	{
+		cin.clear();
+		cin.ignore(10000,'\n');
+		cout << "Is pipe in repair? (1 - Yes and 0 - No) ";
+		cin >> p.InRepair;
+	}while (cin.fail() || ((p.InRepair != 1) && (p.InRepair != 0)));
 	return p;
 };
 
@@ -39,16 +44,15 @@ void PrintPipe(const Pipe& p)
 	cout << "Pipe identifier: " << p.id << endl;
 	cout << "Pipe's diametr: " << p.diametr << endl;
 	cout << "Pipe's length: " << p.length << endl;
-	if (p.InRepair)
-	{
-		cout << "Pipe works" << endl;
-	}
-	else
+	if (p.InRepair == 1)
 	{
 		cout << "Pipe does not work" << endl;
 	}
-	
-}
+	else
+	{
+		cout << "Pipe works" << endl;
+	}
+};
 
 int main()
 {
